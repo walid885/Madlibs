47c286fe3b4e5e55808a66b9f92c8abc7c47dba6
walid885 <bettaieb88walid@gmail.com>
2023-06-21 23:26:29 +0100

    formatting the code, through the madlib

--- program.py
+++ program.py
@@ -1,1 +1,11 @@
-print("hello world !")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
+#well , throughout the vedio, she referenced some code, that generates a madlib, from a ranodom predefined code, 
+#but the main point is just, to , be able, to format the string,, and it is in using the f"" , format
